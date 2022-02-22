793293938f098cd85a1d7ba2e066542c489e464b
Andrea Cleland <andreaclelan@gmail.com>
2022-02-22 12:56:22 -0800

    created workshops and participants tables and connected supabase to the app

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://rkwuoifoqjtdyuamlqbt.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrd3VvaWZvcWp0ZHl1YW1scWJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQzNDE2MTAsImV4cCI6MTk1OTkxNzYxMH0.0bzW8RqDL090ne0FvaYqJLDBsUCtpfGm0oN6I-xwziw';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
