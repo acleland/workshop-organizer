49d0d94b0394c19e87a8abd414e8845b9ed93384
Andrea Cleland <andreaclelan@gmail.com>
2022-02-22 17:28:32 -0800

    implemented renderParticipant and began work on displayWorkshops

--- workshops/workshops.js
+++ workshops/workshops.js
@@ -1,12 +1,34 @@
-import { checkAuth, getUser, getWorkshops, logout } from '../fetch-utils.js';
+import { checkAuth, getUser, getWorkshops } from '../fetch-utils.js';
+import { renderParticipant, renderWorkshop } from '../render-utils.js';
 
 const loginEmail = document.getElementById('login-email');
+const workshopsEl = document.getElementById('workshops');
 
 window.addEventListener('load', async () => {
     checkAuth();
     loginEmail.textContent = getUser().email;
 
-    const workshops = await getWorkshops();
-    console.log(workshops);
-    
+    await displayWorkshops();
+
+
+
 });
+
+async function displayWorkshops() {
+    workshopsEl.textContent = '';
+    const workshops = await getWorkshops();
+
+    for (let workshop of workshops) {
+        const workshopEl = renderWorkshop(workshop);
+        
+        const ul = document.createElement('ul');
+        ul.classList.add('participants');
+        workshopEl.append(ul);
+        for (let participant of workshop.participants) {
+            const li = renderParticipant(participant);
+            ul.append(li);
+        }
+
+        workshopsEl.append(workshopEl);
+    }
+}
